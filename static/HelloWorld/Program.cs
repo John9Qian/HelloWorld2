4464bf929f18f31f825a211e6577b0292f2aa8d5
B Q <qbb1998@outlook.com>
2019-03-05 22:36:45 +0800

    homework

---
+++ static/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void printHelloWorld(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            if(args.Length != 0)
+            {
+                Console.WriteLine("args contain {0} string", args.Length);
+                for(int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine("{0}", args[i]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            printHelloWorld(args);
+            Console.ReadKey();
+        }
+    }
+}
